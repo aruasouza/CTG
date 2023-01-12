a158f9bf08495c6f4d480fe3553b4a33f9d626b3
aruasouza <arua.viso@gmail.com>
2023-01-12 13:44:54 -0300

    tkinter

--- input_montecarlo.py
+++ input_montecarlo.py
@@ -2,6 +2,12 @@ import tkinter as tk
 from tkinter import filedialog
 from azure.datalake.store import core, lib, multithread
 import pandas as pd
+from datetime import datetime
+import time
+import os
+
+# Talvez tenha bugado porque não tem tkinter
+
 tenant = '6e2475ac-18e8-4a6c-9ce5-20cace3064fc'
 RESOURCE = 'https://datalake.azure.net/'
 client_id = "0ed95623-a6d8-473e-86a7-a01009d77232"
@@ -15,14 +21,52 @@ adlCreds = lib.auth(tenant_id = tenant,
 
 adlsFileSystemClient = core.AzureDLFileSystem(adlCreds, store_name=adlsAccountName)
 
+today = datetime.now()
+logfile_name = f'log_{today.month}_{today.year}.csv'
+
+try:
+    multithread.ADLDownloader(adlsFileSystemClient, lpath=logfile_name, 
+        rpath=f'DataLakeRiscoECompliance/LOG/{logfile_name}', nthreads=64, 
+        overwrite=True, buffersize=4194304, blocksize=4194304)
+
+except FileNotFoundError:
+    pd.DataFrame({'time':[],'output':[],'error':[]}).to_csv(logfile_name,index = False)
+    multithread.ADLUploader(adlsFileSystemClient, lpath=logfile_name,
+        rpath=f'DataLakeRiscoECompliance/LOG/{logfile_name}', nthreads=64, overwrite=True, buffersize=4194304, blocksize=4194304)
+
+try:
+    multithread.ADLDownloader(adlsFileSystemClient, lpath='records.csv', 
+        rpath=f'DataLakeRiscoECompliance/LOG/records.csv', nthreads=64, 
+        overwrite=True, buffersize=4194304, blocksize=4194304)
+
+except FileNotFoundError:
+    pd.DataFrame({'file_name':[],'origin':[]}).to_csv('records.csv',index = False)
+    multithread.ADLUploader(adlsFileSystemClient, lpath='records.csv',
+        rpath=f'DataLakeRiscoECompliance/LOG/records.csv', nthreads=64, overwrite=True, buffersize=4194304, blocksize=4194304)
+
+def upload_file_to_directory(file_name,directory):
+    multithread.ADLUploader(adlsFileSystemClient, lpath='output/' + file_name,
+        rpath=f'{directory}/{file_name}', nthreads=64, overwrite=True, buffersize=4194304, blocksize=4194304)
+    time.sleep(1)
+    os.remove('output/' + file_name)
 
 # Capturando o caminho do arquivo csv
-def captura_arquivo ():
+def captura_arquivo():
     # Pedindo o input do nome da previsão
-    Risco=input("Qual o tipo de previsão? (CAMBIO,INFLACAO,JUROS)")
+
     # Escolha do arquivo dentro do computador
     root = tk.Tk()
+    var = tk.StringVar()
+    # var.set("Option 1")  # set the default option
+    option1 = tk.Radiobutton(root, text="CAMBIO", variable=var, value="CAMBIO")
+    option1.pack()
+    option2 = tk.Radiobutton(root, text="INFLACAO", variable=var, value="INFLACAO")
+    option2.pack()
+    option3 = tk.Radiobutton(root, text="JUROS", variable=var, value="JUROS")
+    option3.pack()
+    root.mainloop()
     root.withdraw()
+    Risco = var.get()
     print("Escolha o arquivo para upload")
     file_path = filedialog.askopenfilename()
     # Capturando o arquivo e renomeando para o padrão
@@ -32,14 +76,14 @@ def captura_arquivo ():
     file_name = f'{Risco}_{time_str}.csv'
     # Colocando o output para csv e encaminhando-o para o data lake
     output.to_csv('output/' + file_name)
-    upload_file_to_directory(file_name,f'DataLakeRiscoECompliance/PrevisionData/Variables/{Risco}/AI')
+    upload_file_to_directory(file_name,f'DataLakeRiscoECompliance/PrevisionData/Variables/{Risco}/Manual')
     log = pd.read_csv(logfile_name)
     log = pd.concat([log,pd.DataFrame({'time':[time],'output':[file_name],'error':['no errors']})])
     log.to_csv(logfile_name,index = False)
     multithread.ADLUploader(adlsFileSystemClient, lpath=logfile_name,
         rpath=f'DataLakeRiscoECompliance/LOG/{logfile_name}', nthreads=64, overwrite=True, buffersize=4194304, blocksize=4194304)    
     records = pd.read_csv('records.csv')
-    records = pd.concat([records,pd.DataFrame({'file_name':[file_name]})])
+    records = pd.concat([records,pd.DataFrame({'file_name':[file_name],'origin':['Manual']})])
     records.to_csv('records.csv',index = False)
     multithread.ADLUploader(adlsFileSystemClient, lpath='records.csv',
-        rpath=f'DataLakeRiscoECompliance/LOG/records.csv', nthreads=64, overwrite=True, buffersize=4194304, blocksize=4194304)    
+        rpath=f'DataLakeRiscoECompliance/LOG/records.csv', nthreads=64, overwrite=True, buffersize=4194304, blocksize=4194304)
