af7c98ed5a2c47bbf8474dc524e282fbd9656383
aruasouza <arua.viso@gmail.com>
2023-02-09 17:30:06 -0300

    preco energia arquivo

--- models.py
+++ models.py
@@ -231,7 +231,7 @@ def predict_cambio(test = False,lags = None):
         model = LSTM(y_train,x_train).fit(84,12 * anos)
         # Calculando o Erro
         prediction = model.predict(12 * anos)
-        prediction = pd.Series(prediction).rolling(6,1).mean()
+        prediction = pd.Series(prediction).rolling(6,1).mean().values
         pred_df = cambio.copy()
         pred_df['prediction'] = ([None] * (len(pred_df) - len(prediction))) + list(prediction)
         pred_df['ano'] = pred_df.index.year
@@ -246,6 +246,7 @@ def predict_cambio(test = False,lags = None):
     # Treinando novamente o modelo e calculando o Forecast
     model = RegressionPlusLSTM(cambio,df,square).fit(72,12 * anos)
     prediction = model.predict(12 * anos,regression_level)
+    prediction = pd.Series(prediction).rolling(6,1).mean().values
     pred_df = pd.DataFrame({'prediction':prediction},
         index = pd.period_range(start = cambio.index[-1] + relativedelta(months = 1),periods = len(prediction),freq = 'M'))
     pred_df['superior'] = [pred + (pred * res_max) for pred in prediction]
